a32a8e479d924c341c45c868037439060bdd5976
Maxime Larrieu <43401890+maximelarrieu@users.noreply.github.com>
2018-11-08 17:36:05 +0100

    Update libmatrice.py
    det3, reduit

--- libmatrice.py
+++ libmatrice.py
@@ -10,3 +10,20 @@ def det3(mat):
     Calcul le déterminant d'une matrice 3x3
     '''
     return
+
+
+def reduit(mat, ligne, colonne):
+    '''
+    Calcul d'une matière réduite
+    '''
+    res=[[0,0],[0,0]]
+    ml=0
+    for l in range(3):
+        mc=0
+	if l != ligne:
+            for c in range(3):
+                if c != colonne:
+                    res[ml][mc] = mat[l][c]
+                    mc += 1
+                ml += 1
+    return res
