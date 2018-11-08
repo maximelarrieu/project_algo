6e601b6e585a1b1e30c61a4475433283a9ebb7a7
Maxime Larrieu <43401890+maximelarrieu@users.noreply.github.com>
2018-11-08 16:47:23 +0100

    Create libmatrice.py

---
+++ libmatrice.py
@@ -0,0 +1,12 @@
+def det2(mat):
+    '''
+    Calcul le déterminant d'une matrice 2x2
+    '''
+    return mat[0][0]*mat[1][1]-mat[1][0]*mat[0][1]
+
+
+def det3(mat):
+    '''
+    Calcul le déterminant d'une matrice 3x3
+    '''
+    return
