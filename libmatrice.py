4954010e77894ea72b61f175547f8c5db254f4c0
akhadimer <43401748+akhadimer@users.noreply.github.com>
2018-11-22 14:19:51 +0100

    Thomas

--- libmatrice.py
+++ libmatrice.py
@@ -1,27 +1,20 @@
-def det2(mat):
-    '''
-    Calcul d'un déterminan d'une matrice 2x2
-    '''
-    return mat[0][0]*mat[1][1]-mat[1][0]*mat[0][1]
+def det2(arg):
+    return (arg[0][0] * arg[1][1] - arg[0][1] * arg[1][0])
 
-def det3(mat):
-    '''
-    Calcul d'un déterminant 3x3
-    '''
-    return mat[0][0]*(mat[1][1]*mat[2][2]-mat[2][1]*mat[1][2]) - mat[1][0]*(mat[0][1]*mat[2][2]-mat[2][1]-mat[0][2]) + mat[0][2]*(mat[0][1]*mat[1][2]-mat[1][1]*mat[0][2])
 
 def reduit(mat, ligne, colonne):
-    '''
-    Calcul d'une matrice réduite
-    '''
-    res = [[0, 0], [0, 0]]
+    res = [[0,0],[0,0]]
     ml = 0
     for l in range(3):
         mc = 0
         if l != ligne:
             for c in range(3):
                 if c != colonne:
-                    res[ml][mc] = mat[1][c]
+                    res[ml][mc] = mat[l][c]
                     mc += 1
-                ml += 1
+            ml +=1
     return res
+
+
+def det3(arg):
+    return arg[0][0] * det2(reduit(arg,0,0)) - arg[0][1] * det2(reduit(arg,0,1)) + arg[0][2] * det2(reduit(arg,0,2))
